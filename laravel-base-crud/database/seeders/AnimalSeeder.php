2894da490e91f52b12c044f9942ee95013420d98
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-22 15:47:29 +0200

    creato seeder animali

---
+++ laravel-base-crud/database/seeders/AnimalSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Animal;
+use Faker\Factory as Faker;
+
+class AnimalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 50) as $index) {
+            Animal::create([
+                'name' => $faker->name,
+                'species' => $faker->word,
+            ]);
+        }
+    }
+}
+
