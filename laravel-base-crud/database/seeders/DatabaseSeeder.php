e3b5eef2b330dac1b68462f78d8dce6dedbf2291
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-23 09:34:59 +0200

    errore non risolto: Undefined variable $animals

--- laravel-base-crud/database/seeders/DatabaseSeeder.php
+++ laravel-base-crud/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(AnimalSeeder::class);
+        $this->call(AnimalsTableSeeder::class);
     }
 }
 
--- laravel-base-crud/routes/web.php
+++ laravel-base-crud/routes/web.php
@@ -13,5 +13,8 @@ use App\Http\Controllers\AnimalController;
 |
 */
 
+Route::get('/', function () {
+    return view('animals.index');
+});
 
 Route::resource('animals', AnimalController::class);
